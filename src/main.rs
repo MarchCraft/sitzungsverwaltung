6f450e1aaa9dffc864a0d9982a5d9666a3eade9a
Felix Nilles <felix@dienilles.de>
2024-04-07 01:58:37 +0200

    update browsing works

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-const URL: &str = "http://localhost:8080/";
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Sitzung {
-    name: String,
-    datum: NaiveDateTime,
-    id: Uuid,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Top {
-    name: String,
-    id: Uuid,
-    inhalt: String,
-    weight: i32,
-}
-
-fn get_sitzungen() -> Vec<Sitzung> {
-    let endoint = "api/topmanager/sitzungen/";
-    let reqwest = reqwest::blocking::Client::new();
-    let response = reqwest.get(URL.to_string() + endoint).send().unwrap();
-    let sitzungen: Vec<Sitzung> = response.json().unwrap();
-    sitzungen
-}
-
-use std::{error::Error, io, io::stdout};
-
 use chrono::NaiveDateTime;
 use color_eyre::config::HookBuilder;
 use crossterm::{
@@ -35,14 +9,14 @@ use crossterm::{
 };
 use ratatui::{prelude::*, style::palette::tailwind, widgets::*};
 use serde::{Deserialize, Serialize};
+use std::{error::Error, io, io::stdout};
 use uuid::Uuid;
 
 const TODO_HEADER_BG: Color = tailwind::BLUE.c950;
 const NORMAL_ROW_COLOR: Color = tailwind::SLATE.c950;
-const ALT_ROW_COLOR: Color = tailwind::SLATE.c900;
 const SELECTED_STYLE_FG: Color = tailwind::BLUE.c300;
 const TEXT_COLOR: Color = tailwind::SLATE.c200;
-const COMPLETED_TEXT_COLOR: Color = tailwind::GREEN.c500;
+const URL: &str = "http://localhost:8080/";
 
 struct StatefulList<T> {
     state: ListState,
@@ -54,26 +28,51 @@ struct StatefulList<T> {
 enum SelectedLayout {
     Sitzungen,
     Tops,
+    Anträge,
 }
 
-/// This struct holds the current state of the app. In particular, it has the `items` field which is
-/// a wrapper around `ListState`. Keeping track of the items state let us render the associated
-/// widget with its state and have access to features such as natural scrolling.
-///
-/// Check the event handling at the bottom to see how to change the state on incoming events.
-/// Check the drawing logic for items on how to specify the highlighting style for selected items.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Sitzung {
+    name: String,
+    datum: NaiveDateTime,
+    id: Uuid,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Top {
+    name: String,
+    id: Uuid,
+    inhalt: serde_json::Value,
+    weight: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Antrag {
+    id: Uuid,
+    titel: String,
+    begründung: String,
+    antragstext: String,
+}
+
+fn get_sitzungen() -> Vec<Sitzung> {
+    let endoint = "api/topmanager/sitzungen/";
+    let reqwest = reqwest::blocking::Client::new();
+    let response = reqwest.get(URL.to_string() + endoint).send().unwrap();
+    let sitzungen: Vec<Sitzung> = response.json().unwrap();
+    sitzungen
+}
+
 struct App {
     sitzungen: StatefulList<Sitzung>,
     tops_selected_sitzung: StatefulList<Top>,
+    anträge_selected_top: StatefulList<Antrag>,
     layout: SelectedLayout,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // setup terminal
     init_error_hooks()?;
     let terminal = init_terminal()?;
 
-    // create app and run it
     App::new().run(terminal)?;
 
     restore_terminal()?;
@@ -115,16 +114,12 @@ impl<'a> App {
         Self {
             sitzungen: StatefulList::with_items(get_sitzungen()),
             tops_selected_sitzung: StatefulList::with_items(vec![]),
+            anträge_selected_top: StatefulList::with_items(vec![]),
             layout: SelectedLayout::Sitzungen,
         }
     }
 
-    /// Changes the status of the selected list ite
-    fn go_top(&mut self) {
-        self.sitzungen.state.select(Some(0));
-    }
-
-    fn open_sitzung(&mut self, buffer: &mut Buffer) {
+    fn open_sitzung(&mut self) {
         let selected = self.sitzungen.state.selected().unwrap();
         let sitzung = self.sitzungen.items[selected].clone();
         let url = format!("{}api/topmanager/sitzung/{}/tops/", URL, sitzung.id);
@@ -136,10 +131,20 @@ impl<'a> App {
         self.layout = SelectedLayout::Tops;
     }
 
-    fn go_bottom(&mut self) {
-        self.sitzungen
-            .state
-            .select(Some(self.sitzungen.items.len() - 1));
+    fn open_top(&mut self) {
+        let selected = self.tops_selected_sitzung.state.selected().unwrap();
+        let top = self.tops_selected_sitzung.items[selected].clone();
+        let url = format!("{}api/topmanager/tops/{}/anträge/", URL, top.id);
+        let reqwest = reqwest::blocking::Client::new();
+        let response = reqwest.get(url).send().unwrap();
+        let antrag: Vec<Antrag> = response.json().unwrap();
+        //open new view with top
+        self.anträge_selected_top = StatefulList::with_items(antrag);
+        self.layout = SelectedLayout::Anträge;
+    }
+
+    fn exit_app(&self) {
+        std::process::exit(0);
     }
 }
 
@@ -148,20 +153,12 @@ impl App {
         loop {
             self.draw(&mut terminal)?;
 
-            if let Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press {
-                    use KeyCode::*;
-                    match key.code {
-                        Char('q') | Esc => return Ok(()),
-                        Char('h') | Left => self.sitzungen.unselect(),
-                        Char('j') | Down => self.sitzungen.next(),
-                        Char('k') | Up => self.sitzungen.previous(),
-                        Char('o') => self.open_sitzung(terminal.current_buffer_mut()),
-                        Char('g') => self.go_top(),
-                        Char('G') => self.go_bottom(),
-                        _ => {}
-                    }
-                }
+            if let SelectedLayout::Sitzungen = self.layout {
+                self.handle_sitzungen(&mut terminal)?;
+            } else if let SelectedLayout::Tops = self.layout {
+                self.handle_tops(&mut terminal)?;
+            } else {
+                self.handle_anträge(&mut terminal)?;
             }
         }
     }
@@ -170,11 +167,64 @@ impl App {
         terminal.draw(|f| f.render_widget(self, f.size()))?;
         Ok(())
     }
+
+    fn handle_sitzungen(&mut self, terminal: &mut Terminal<impl Backend>) -> io::Result<()> {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                use KeyCode::*;
+                match key.code {
+                    Char('q') | Esc => self.exit_app(),
+                    Char('h') | Left => self.sitzungen.unselect(),
+                    Char('j') | Down => self.sitzungen.next(),
+                    Char('k') | Up => self.sitzungen.previous(),
+                    Char('o') => self.open_sitzung(),
+                    _ => {}
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn handle_tops(&mut self, terminal: &mut Terminal<impl Backend>) -> io::Result<()> {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                use KeyCode::*;
+                match key.code {
+                    Char('q') | Esc => self.switch_layout(SelectedLayout::Sitzungen),
+                    Char('h') | Left => self.tops_selected_sitzung.unselect(),
+                    Char('j') | Down => self.tops_selected_sitzung.next(),
+                    Char('k') | Up => self.tops_selected_sitzung.previous(),
+                    Char('o') => self.open_top(),
+                    _ => {}
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn handle_anträge(&mut self, terminal: &mut Terminal<impl Backend>) -> io::Result<()> {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                use KeyCode::*;
+                match key.code {
+                    Char('q') | Esc => self.switch_layout(SelectedLayout::Tops),
+                    Char('h') | Left => self.anträge_selected_top.unselect(),
+                    Char('j') | Down => self.anträge_selected_top.next(),
+                    Char('k') | Up => self.anträge_selected_top.previous(),
+                    _ => {}
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn switch_layout(&mut self, layout: SelectedLayout) {
+        self.layout = layout;
+    }
 }
 
 impl Widget for &mut App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        // Create a space for header, todo list and the footer.
         let vertical = Layout::vertical([
             Constraint::Length(2),
             Constraint::Min(0),
@@ -182,18 +232,56 @@ impl Widget for &mut App {
         ]);
         let [header_area, rest_area, footer_area] = vertical.areas(area);
 
-        // Create two chunks with equal vertical screen space. One for the list and the other for
-        // the info block.
-
         render_title(header_area, buf);
-        self.render_overview_sitzungen(rest_area, buf);
+        if let SelectedLayout::Sitzungen = self.layout {
+            self.render_overview_sitzungen(rest_area, buf);
+        } else if let SelectedLayout::Tops = self.layout {
+            self.render_overview_tops(rest_area, buf);
+        } else {
+            self.render_anträge(rest_area, buf);
+        }
         render_footer(footer_area, buf);
     }
 }
 
 impl App {
+    fn render_overview(
+        &self,
+        area: Rect,
+        buf: &mut Buffer,
+        title: String,
+        items: Vec<String>,
+        state: &mut ListState,
+    ) {
+        let outer_block = Block::default()
+            .borders(Borders::NONE)
+            .fg(TEXT_COLOR)
+            .bg(TODO_HEADER_BG)
+            .title(title)
+            .title_alignment(Alignment::Center);
+        let inner_block = Block::default()
+            .borders(Borders::NONE)
+            .fg(TEXT_COLOR)
+            .bg(NORMAL_ROW_COLOR);
+
+        let outer_area = area;
+        let inner_area = outer_block.inner(outer_area);
+
+        outer_block.render(outer_area, buf);
+        let items = List::new(items)
+            .block(inner_block)
+            .highlight_style(
+                Style::default()
+                    .add_modifier(Modifier::BOLD)
+                    .add_modifier(Modifier::REVERSED)
+                    .fg(SELECTED_STYLE_FG),
+            )
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
+
+        StatefulWidget::render(items, inner_area, buf, state);
+    }
     fn render_overview_sitzungen(&mut self, area: Rect, buf: &mut Buffer) {
-        // We create two blocks, one is for the header (outer) and the other is for list (inner).
         let outer_block = Block::default()
             .borders(Borders::NONE)
             .fg(TEXT_COLOR)
@@ -205,11 +293,9 @@ impl App {
             .fg(TEXT_COLOR)
             .bg(NORMAL_ROW_COLOR);
 
-        // We get the inner area from outer_block. We'll use this area later to render the table.
         let outer_area = area;
         let inner_area = outer_block.inner(outer_area);
 
-        // We can render the header in outer_area.
         outer_block.render(outer_area, buf);
 
         let sitzungen = self.sitzungen.items.iter().map(|sitzung| {
@@ -217,7 +303,6 @@ impl App {
             ListItem::new(text)
         });
 
-        // Create a List from all list items and highlight the currently selected one
         let items = List::new(sitzungen)
             .block(inner_block)
             .highlight_style(
@@ -229,12 +314,91 @@ impl App {
             .highlight_symbol(">")
             .highlight_spacing(HighlightSpacing::Always);
 
+        StatefulWidget::render(items, inner_area, buf, &mut self.sitzungen.state);
+    }
+
+    fn render_overview_tops(&mut self, area: Rect, buf: &mut Buffer) {
+        // We create two blocks, one is for the header (outer) and the other is for list (inner).
+        let outer_block = Block::default()
+            .borders(Borders::NONE)
+            .fg(TEXT_COLOR)
+            .bg(TODO_HEADER_BG)
+            .title("TODO List")
+            .title_alignment(Alignment::Center);
+        let inner_block = Block::default()
+            .borders(Borders::NONE)
+            .fg(TEXT_COLOR)
+            .bg(NORMAL_ROW_COLOR);
+        // We get the inner area from outer_block. We'll use this area later to render the table.
+        let outer_area = area;
+        let inner_area = outer_block.inner(outer_area);
+        // We can render the header in outer_area.
+        outer_block.render(outer_area, buf);
+        let tops = self.tops_selected_sitzung.items.iter().map(|top| {
+            let text = format!("{} {}", top.weight as i32, top.name);
+            ListItem::new(text)
+        });
+        // Create a List from all list items and highlight the currently selected one
+        let items = List::new(tops)
+            .block(inner_block)
+            .highlight_style(
+                Style::default()
+                    .add_modifier(Modifier::BOLD)
+                    .add_modifier(Modifier::REVERSED)
+                    .fg(SELECTED_STYLE_FG),
+            )
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
         // We can now render the item list
         // (look careful we are using StatefulWidget's render.)
         // ratatui::widgets::StatefulWidget::render as stateful_render
-        StatefulWidget::render(items, inner_area, buf, &mut self.sitzungen.state);
+        StatefulWidget::render(
+            items,
+            inner_area,
+            buf,
+            &mut self.tops_selected_sitzung.state,
+        );
+    }
+
+    fn render_anträge(&mut self, area: Rect, buf: &mut Buffer) {
+        // We create two blocks, one is for the header (outer) and the other is for list (inner).
+        let outer_block = Block::default()
+            .borders(Borders::NONE)
+            .fg(TEXT_COLOR)
+            .bg(TODO_HEADER_BG)
+            .title("TODO List")
+            .title_alignment(Alignment::Center);
+        let inner_block = Block::default()
+            .borders(Borders::NONE)
+            .fg(TEXT_COLOR)
+            .bg(NORMAL_ROW_COLOR);
+        // We get the inner area from outer_block. We'll use this area later to render the table.
+        let outer_area = area;
+        let inner_area = outer_block.inner(outer_area);
+        // We can render the header in outer_area.
+        outer_block.render(outer_area, buf);
+        let anträge = self.anträge_selected_top.items.iter().map(|antrag| {
+            let text = format!("{} {}", antrag.titel, antrag.begründung);
+            ListItem::new(text)
+        });
+        // Create a List from all list items and highlight the currently selected one
+        let items = List::new(anträge)
+            .block(inner_block)
+            .highlight_style(
+                Style::default()
+                    .add_modifier(Modifier::BOLD)
+                    .add_modifier(Modifier::REVERSED)
+                    .fg(SELECTED_STYLE_FG),
+            )
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
+        // We can now render the item list
+        // (look careful we are using StatefulWidget's render.)
+        // ratatui::widgets::StatefulWidget::render as stateful_render
+        StatefulWidget::render(items, inner_area, buf, &mut self.anträge_selected_top.state);
     }
 }
+
 fn render_title(area: Rect, buf: &mut Buffer) {
     Paragraph::new("Ratatui List Example")
         .bold()
@@ -263,7 +427,7 @@ impl<T> StatefulList<T> {
     fn next(&mut self) {
         let i = match self.state.selected() {
             Some(i) => {
-                if i >= self.items.len() - 1 {
+                if self.items.len() == 1 || self.items.len() - 1 == i {
                     0
                 } else {
                     i + 1
